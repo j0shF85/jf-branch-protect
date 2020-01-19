c5ad4c2bce8dacdd47d02826d0c1be1c13126b9f
Josh Feng <joshfeng@Joshs-MBP.modem>
2020-01-19 23:08:10 +1100

    protect master branch for approvers before merging

--- index.js
+++ index.js
@@ -5,10 +5,29 @@
 module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
-
+  /*
   app.on('issues.opened', async context => {
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
     return context.github.issues.createComment(issueComment)
+  })
+  */
+
+  // protect master branch when repo get created
+  app.on('repository.created', async context => {
+    // protect master branch
+    const createBranchProtectionRule = `
+      mutation repository($id: ID!, $pattern: String!, $requiresApprovingReviews: Boolean!, $requiredApprovingReviewCount: Int!) {
+        createBranchProtectionRule(input: {repositoryId: $id, pattern: $pattern, requiresApprovingReviews: $requiresApprovingReviews, requiredApprovingReviewCount: $requiredApprovingReviewCount}) {
+          clientMutationId
+        }
+      }
+    `
+    context.github.graphql(createBranchProtectionRule, {
+      id: context.payload.repository.node_id,
+      pattern: "master",
+      requiresApprovingReviews: true,
+      requiredApprovingReviewCount: 2
+    })
   })
 
   // For more information on building apps:
@@ -16,4 +35,5 @@ module.exports = app => {
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
+
 }
